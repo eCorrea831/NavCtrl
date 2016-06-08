0e7903c955b661d9fac7df2066aecd9ea40964b1
RoseMax <erica.correa831@gmail.com>
2016-06-08 13:30:52 -0400

    - Add collectionVC for company and product and matching xibs - Add groups to organize project and seaparte tableVC from colelctionVC

---
+++ NavCtrl/CompanyCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CompanyCollectionViewController.h
+//  NavCtrl
+//
+//  Created by Aditya Narayan on 6/8/16.
+//  Copyright © 2016 Aditya Narayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompanyCollectionViewController : UICollectionViewController
+
+@end
