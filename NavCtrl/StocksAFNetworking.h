9c724a458cefb6c5b8e8983bfa9b68ec1a9efc7d
RoseMax <erica.correa831@gmail.com>
2016-06-10 14:29:16 -0400

    Add AFNetworking library to project

---
+++ NavCtrl/StocksAFNetworking.h
@@ -0,0 +1,13 @@
+//
+//  StocksAFNetworking.h
+//  NavCtrl
+//
+//  Created by Aditya Narayan on 6/10/16.
+//  Copyright © 2016 Aditya Narayan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StocksAFNetworking : NSObject
+
+@end
---
+++ NavCtrl/StocksAFNetworking.m
@@ -0,0 +1,16 @@
+//
+//  StocksAFNetworking.m
+//  NavCtrl
+//
+//  Created by Aditya Narayan on 6/10/16.
+//  Copyright © 2016 Aditya Narayan. All rights reserved.
+//
+
+#import "StocksAFNetworking.h"
+
+
+@implementation StocksAFNetworking
+
+
+
+@end
